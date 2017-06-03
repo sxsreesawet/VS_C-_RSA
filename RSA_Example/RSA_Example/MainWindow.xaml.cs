9d1a6ce213889f3dea7ef6679b24119344c0a6f5
sxsreesawet <s.sreesawet@gmail.com>
2017-06-02 20:16:08 -0500

    need to check the else statement

---
+++ RSA_Example/RSA_Example/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSA_Example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ulong N = 0;
+        ulong P = 0;
+        ulong Q = 0;
+        int i = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Title = "RSA Encryption";
+        }
+
+        private void txtP1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool P_numeric = ulong.TryParse(txtP1.Text, out P);
+            if (P_numeric)
+            {
+                bool Q_numeric = ulong.TryParse(txtP2.Text, out Q);
+                if (Q_numeric)
+                {
+                    N = P * Q;
+                    cmdGenerate.IsEnabled = true;
+                    lbl1.Content = "drop up allow";
+                }
+                else
+                {
+                    cmdGenerate.IsEnabled = false;
+                    lbl1.Content = "drop up block";
+                }
+            }  
+          
+
+
+
+        }
+
+
+
+        private void txtP2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bool Q_numeric = ulong.TryParse(txtP2.Text, out Q);
+            if (Q_numeric)
+            {
+                bool P_numeric = ulong.TryParse(txtP1.Text, out P);
+                if (P_numeric)
+                {
+                    N = P * Q;
+                    cmdGenerate.IsEnabled = true;
+                    lbl1.Content = "drop down allow";
+                }
+                else
+                {
+                    cmdGenerate.IsEnabled = false;
+                    lbl1.Content = "drop down block";
+                }
+            }
+            
+        }
+    }
+}
